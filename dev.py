1b771c498a194e129ed2f50fefbe659b7b684cce
sideous <jvanmuck@yahoo.com>
2020-09-14 16:08:09 -0700

    Added user input and comments

--- dev.py
+++ dev.py
@@ -10,18 +10,33 @@ def llres(r1,r2):
     req=req/(r1+r2)
     return req
     
+
+# Get user input  
+print('Enter output voltage as a decimal of the input voltage:')
+nx =input()
+print ('Output =', nx, ' * input votage')
+print ('Enter max value of divider:')
+divmax=input()
+print ('Enter max value of divider:')
+divmin=input()
+
+# Build arrays
 with open('rs.csv', 'r') as file:
     reader = csv.reader(file)
     i=0
     res=array('f',[0.0])
     synres=array('f',[0.0])
+    #Build res[] from values in csv
     for each_row in reader:
         res[i]=float(each_row[0])
         res.extend([float(each_row[0])])
         print(res[i], i)
         i=i+1
     file.close()
+
+
     k=0
+    #Build array of all permitations of parallel combinations
 for i in range(len(res)):
     print(i)
     for j in range(len(res)):
