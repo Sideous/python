1b25b19046938d0e85850d2b805e3a3898d7339f
sideous <jvanmuck@yahoo.com>
2020-09-15 16:17:52 -0700

    finds z1 sort of

--- dev.py
+++ dev.py
@@ -13,12 +13,12 @@ def llres(r1,r2):
 
 # Get user input  
 print('Enter output voltage as a decimal of the input voltage:')
-nx =input()
-print ('Output =', nx, ' * input votage')
+nx =float(input())
+#print ('Output =', nx, ' * input votage')
 print ('Enter max value of divider:')
-divmax=input()
-print ('Enter max value of divider:')
-divmin=input()
+divmax=float(input())
+print ('Enter min value of divider:')
+divmin=float(input())
 
 # Build arrays
 with open('rs.csv', 'r') as file:
@@ -32,17 +32,48 @@ with open('rs.csv', 'r') as file:
         res.extend([float(each_row[0])])
         print(res[i], i)
         i=i+1
-    file.close()
 
-
-    k=0
-    #Build array of all permitations of parallel combinations
+file.close()
+#res.sort()
+res.pop(i)
+    
+k=0
+#Build array of all permitations of parallel combinations
 for i in range(len(res)):
-    print(i)
     for j in range(len(res)):
         synres[k]=llres(res[i], res[j])
         synres.extend([synres[k]])
         k=k+1
         print(synres[k], k)
+
+import math
+
+#Begin search
+factor=1+nx/(1-nx)
+z1_max=divmax/factor
+z1_min=divmin/factor
+
+
+
+for z1_target in range(math.floor(z1_max),math.floor(z1_min),-1):
+    z1=0
+    temp_str=""
+    for i in range(len(res)):
+        if z1 > (0.99 * z1_target):
+            break
+        passes= math.floor(z1_target/res[i])-1
+        if passes >= 0:
+            for j in range(passes):
+                z1=z1+res[i]
+                temp_str=temp_str + str(res[i])
+                temp_str=temp_str + ", "
+                print("j=",j)
+    print(temp_str)
         
+    
+temp = "start"
+temp = temp + ", "
+temp = temp + str(synres[0])
+print(temp)
+#math.floor()
 print("hello", len(res))
